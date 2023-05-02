e0fe2332c19f3184024219fdf6e56beded4acc6b
Tiara Lestari S <36650508+tiaralestaris@users.noreply.github.com>
2023-05-02 22:20:45 +0700

    feat: setup translate feature

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './helpers/i18n';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 );
 
